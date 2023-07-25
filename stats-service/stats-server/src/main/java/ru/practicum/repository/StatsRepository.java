87085c24bfd200f9e270552f4819fab61adcad3b
avtuman1 <avtuman1@mts.ru>
2023-07-26 01:02:36 +0300

    style: style errors corrected

--- stats-service/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -12,32 +12,32 @@ import java.util.List;
 public interface StatsRepository extends JpaRepository<Hit, Integer> {
 
     @Query("select new ru.practicum.model.StatDb(h.app, h.uri, COUNT(h.ip))" +
-            "from Hit as h "+
-            "where h.created > :startDt and h.created < :endDt " +
-            "and h.uri IN (:uris) " +
-            "group by h.app, h.uri "+
-            "order by 3 desc")
-    List<StatDb> getHitsByAppAndUris(LocalDateTime startDt, LocalDateTime endDt, @Param("uris")List<String> uris);
-
-    @Query("select new ru.practicum.model.StatDb(h.app, h.uri, COUNT(distinct h.ip))" +
-            "from Hit as h "+
+            "from Hit as h " +
             "where h.created > :startDt and h.created < :endDt " +
             "and h.uri IN (:uris) " +
             "group by h.app, h.uri " +
             "order by 3 desc")
-    List<StatDb> getHitsByAppAndUrisUnique(LocalDateTime startDt, LocalDateTime endDt, @Param("uris")List<String> uris);
+    List<StatDb> getHitsByAppAndUris(LocalDateTime startDt, LocalDateTime endDt, @Param("uris") List<String> uris);
+
+    @Query("select new ru.practicum.model.StatDb(h.app, h.uri, COUNT(distinct h.ip))" +
+            "from Hit as h " +
+            "where h.created > :startDt and h.created < :endDt " +
+            "and h.uri IN (:uris) " +
+            "group by h.app, h.uri " +
+            "order by 3 desc")
+    List<StatDb> getHitsByAppAndUrisUnique(LocalDateTime startDt, LocalDateTime endDt, @Param("uris") List<String> uris);
 
     @Query("select new ru.practicum.model.StatDb(h.app, h.uri, COUNT(h.ip))" +
-            "from Hit as h "+
+            "from Hit as h " +
             "where h.created > :startDt and h.created < :endDt " +
-            "group by h.app, h.uri "+
+            "group by h.app, h.uri " +
             "order by 3 desc")
     List<StatDb> getHitsUnique(LocalDateTime startDt, LocalDateTime endDt);
 
     @Query("select new ru.practicum.model.StatDb(h.app, h.uri, COUNT(distinct h.ip))" +
-            "from Hit as h "+
+            "from Hit as h " +
             "where h.created > :startDt and h.created < :endDt " +
-            "group by h.app, h.uri "+
+            "group by h.app, h.uri " +
             "order by 3 desc")
     List<StatDb> getHits(LocalDateTime startDt, LocalDateTime endDt);
 }
